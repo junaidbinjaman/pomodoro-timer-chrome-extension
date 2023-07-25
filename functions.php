b1f877689acd234e8f3b65f3fe0347c83715e135
junaidbinjaman <me@junaidbinjaman.com>
2023-07-25 11:32:49 +0600

    Initialized food delivery app

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * 
+ * Project: Food delivery app
+ * Author: Junaid Bin Jaman
+ * Date: July, 25, 2023
+ * Version: 1.0.0
+ * 
+ * This functions.php file is the main configurations file for this system
+ * In this project, I have 3 folders admin, includes, and public.
+ * admin folder is where all the admin related files live in
+ * public is the folder where all the public facing files lives
+ * includes is the folder where all the multi functioning file lives
+ */
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * 
+ * Project: Food delivery app
+ * Package: Home page
+ * Author: Junaid Bin Jaman
+ * Date: July, 24, 2023
+ * Version: 1.0.0
+ * 
+ */
+
+ require_once "../config.php"
+
+ ?>
+
+ <h1>Hello, World</h1>
