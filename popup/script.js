d2b12ae40a1864b478bc23bf732a050b65f59703
junaidbinjaman <me@junaidbinjaman.com>
2024-04-29 15:15:46 +0600

    T

--- popup/script.js
+++ popup/script.js
@@ -1,35 +1,84 @@
-const tasks = [];
+let tasks = [];
+
+const startTimerBtn = document.getElementById("start-timer-btn");
+
+startTimerBtn.addEventListener('click', () => {
+    chrome.storage.local.get(["isRunning"], (res) => {
+        chrome.storage.local.set({
+            isRunning: !res.isRunning
+        }, () => {
+            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
+        })
+    })
+})
+
+const resetBtn = document.getElementById('reset-timer-btn');
+resetBtn.addEventListener('click', () => {
+    chrome.storage.local.set({
+        timer: 0,
+        isRunning: false
+    }, () => {
+        startTimerBtn.textContent = "Start Timer";
+    })
+})
 
 const addTaskBtn = document.getElementById('add-task-btn');
+addTaskBtn.addEventListener('click', () => addTask());
 
-addTaskBtn.addEventListener('click', () => addTask()) ;
+chrome.storage.sync.get(['tasks'], (res) => {
+    tasks = res.tasks ? res.tasks : [];
+    renderTasks();
+})
+
+function saveTasks() {
+    chrome.storage.sync.set({
+        tasks,
+    })
+}
+
+function renderTask(taskNum) {
+  const taskRow = document.createElement('div');
+
+  const text = document.createElement('input');
+  text.type = 'text';
+  text.placeholder = 'Enter a task...';
+  text.value = tasks[taskNum];
+  text.addEventListener('change', () => {
+    tasks[taskNum] = text.value;
+    saveTasks();
+  });
+
+  const deleteBtn = document.createElement('input');
+  deleteBtn.type = 'button';
+  deleteBtn.value = 'X';
+  deleteBtn.addEventListener('click', () => {
+    deleteTask(taskNum);
+  });
+
+  taskRow.appendChild(text);
+  taskRow.appendChild(deleteBtn);
+
+  const taskContainer = document.getElementById('task-container');
+  taskContainer.appendChild(taskRow);
+}
 
 function addTask() {
-    const taskNum = tasks.length;
-    tasks.push("")
-    
-    const taskRow = document.createElement('div');
-    
-    const text = document.createElement('input');
-    text.type = 'text';
-    text.placeholder = 'Enter a task...';
-    text.addEventListener("change", () => {
-        tasks[taskNum] = text.value;
-        console.log(tasks);
-    })
+  const taskNum = tasks.length;
+  tasks.push('');
+  renderTask(taskNum);
+  saveTasks();
+}
 
-    const deleteBtn = document.createElement('input');
-    deleteBtn.type = 'button';
-    deleteBtn.value = 'X';
-    deleteBtn.addEventListener('click', () => {
-        tasks.splice(taskNum, 1);
-        console.log(tasks);
-    })
+function deleteTask(taskNum) {
+    tasks.splice(taskNum, 1);
+    renderTasks(taskNum);
+    saveTasks();
+}
 
-    taskRow.appendChild(text);
-    taskRow.appendChild(deleteBtn);
-
+function renderTasks() {
     const taskContainer = document.getElementById('task-container');
-    taskContainer.appendChild(taskRow);
-
+    taskContainer.textContent = "";
+    tasks.forEach((task, taskNum) => {
+        renderTask(taskNum);
+    })
 }
