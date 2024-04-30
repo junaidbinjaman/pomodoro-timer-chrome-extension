5ca7ca34e3438fc068a3521fea65bb0fcc21140b
junaidbinjaman <me@junaidbinjaman.com>
2024-04-30 08:41:18 +0600

    The pomodoro functionality is created. It's time to style it a bit

--- background.js
+++ background.js
@@ -6,21 +6,32 @@ chrome.alarms.create("pomodoroTimer", {
 // Event listener for the alarm
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (alarm.name === "pomodoroTimer") {
-        chrome.storage.local.get(["timer", "isRunning"], (res) => {
+        chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
             if (res.isRunning) {
                 let timer = res.timer + 1;
-                console.log(timer);
+                let isRunning = true;
+
+                 if (timer === 60 * res.timeOption) {
+                    this.registration.showNotification("Pomodoro Timer", {
+                        body: `${res.timeOption} minutes  has passed`,
+                        icon: "/images/icon.png"
+                    })
+                    timer = 0;
+                    isRunning = false;
+                 }
                 chrome.storage.local.set({
-                    timer
+                    timer,
+                    isRunning
                 })
             }
         })
     }
 });
 
-chrome.storage.local.get(["timer", "isRunning"], (res) => {
+chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
     chrome.storage.local.set({
         timer: "timer" in res ? res.timer : 0,
+        timeOption: "timeOption" in res ? res.timeOption : 25, 
         isRunning: "isRunning" in res ? res.isRunning : false
     })
 })
--- popup/script.js
+++ popup/script.js
@@ -1,39 +1,62 @@
 let tasks = [];
 
-const startTimerBtn = document.getElementById("start-timer-btn");
+function updateTime() {
+  chrome.storage.local.get(["timer", "timeOption"], (res) => {
+    const time = document.getElementById('time');
+    const minute = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2, '0');
+    let second = '00';
+    if (res.timer % 60 != 0) {
+      second = `${60 - (res.timer % 60)}`.padStart(2, '0');
+    }
+    time.textContent = `${minute}:${second}`;
+  });
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+const startTimerBtn = document.getElementById('start-timer-btn');
 
 startTimerBtn.addEventListener('click', () => {
-    chrome.storage.local.get(["isRunning"], (res) => {
-        chrome.storage.local.set({
-            isRunning: !res.isRunning
-        }, () => {
-            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
-        })
-    })
-})
+  chrome.storage.local.get(['isRunning'], (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning
+          ? 'Pause Timer'
+          : 'Start Timer';
+      }
+    );
+  });
+});
 
 const resetBtn = document.getElementById('reset-timer-btn');
 resetBtn.addEventListener('click', () => {
-    chrome.storage.local.set({
-        timer: 0,
-        isRunning: false
-    }, () => {
-        startTimerBtn.textContent = "Start Timer";
-    })
-})
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = 'Start Timer';
+    }
+  );
+});
 
 const addTaskBtn = document.getElementById('add-task-btn');
 addTaskBtn.addEventListener('click', () => addTask());
 
 chrome.storage.sync.get(['tasks'], (res) => {
-    tasks = res.tasks ? res.tasks : [];
-    renderTasks();
-})
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
 
 function saveTasks() {
-    chrome.storage.sync.set({
-        tasks,
-    })
+  chrome.storage.sync.set({
+    tasks,
+  });
 }
 
 function renderTask(taskNum) {
@@ -70,15 +93,15 @@ function addTask() {
 }
 
 function deleteTask(taskNum) {
-    tasks.splice(taskNum, 1);
-    renderTasks(taskNum);
-    saveTasks();
+  tasks.splice(taskNum, 1);
+  renderTasks(taskNum);
+  saveTasks();
 }
 
 function renderTasks() {
-    const taskContainer = document.getElementById('task-container');
-    taskContainer.textContent = "";
-    tasks.forEach((task, taskNum) => {
-        renderTask(taskNum);
-    })
+  const taskContainer = document.getElementById('task-container');
+  taskContainer.textContent = '';
+  tasks.forEach((task, taskNum) => {
+    renderTask(taskNum);
+  });
 }
